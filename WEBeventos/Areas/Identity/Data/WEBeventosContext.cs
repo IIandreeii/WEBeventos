f79f97f9fade64426db9e73ebeb9a047e58a15ac
IIandreeii <wilman1538@outlook.es>
2023-08-23 22:15:11 -0600

    first commit

---
+++ WEBeventos/Areas/Identity/Data/WEBeventosContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WEBeventos.Areas.Identity.Data;
+
+namespace WEBeventos.Data;
+
+public class WEBeventosContext : IdentityDbContext<WEBeventosUser>
+{
+    public WEBeventosContext(DbContextOptions<WEBeventosContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ WEBeventos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WEBeventos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
